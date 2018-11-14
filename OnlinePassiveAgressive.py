1d4502426a9272eb12a6292b0bf4f50cfea862dc
Aboud <aboud.zakaria@gmail.com>
2018-11-14 18:41:23 +0100

    introduce noise

--- OnlinePassiveAgressive.py
+++ OnlinePassiveAgressive.py
@@ -1,8 +1,13 @@
 """ Online Passive Agressive class. """
 
+import random
 from time import time
 import numpy as np
 import pandas as pd
+from sklearn import svm
+
+random.seed(777)
+np.random.seed(777)
 
 
 class OnlinePassiveAgressive:
@@ -56,7 +61,6 @@ if __name__ == '__main__':
 
     # get numpy array
     data = df.values
-    np.random.seed(777)
     np.random.shuffle(data)
 
     labels = data[:, 0] * 2 - 3  # from (1,2) to (+1,-1) classification
@@ -72,7 +76,9 @@ if __name__ == '__main__':
     X_test = data[trainsize:]
     y_test = labels[trainsize:]
 
-    clf = OnlinePassiveAgressive(d, tao=2, C=2)
+    ### OPA
+
+    clf = OnlinePassiveAgressive(d, tao=2, C=200)
 
     correct = 0
     start = time()
@@ -81,16 +87,54 @@ if __name__ == '__main__':
         correct += int(y_train[i] == yihat)
         clf.online_learn(X_train[i], y_train[i])
 
-    print(f'train time elapsed: {(time() - start) * 100}ms')
-    print(f'train online accuracy: {correct * 100 // trainsize}%')
+    print(f'OPA train time elapsed: {(time() - start) * 100}ms')
+    print(f'OPA train online accuracy: {correct * 100 // trainsize}%')
 
     correct = 0
     start = time()
     for i in range(testsize):
         yihat = clf.predict(X_test[i])
         correct += int(y_test[i] == yihat)
-        # do not learn from testing dataset
-        #clf.online_learn(X_test[i], y_test[i])
 
-    print(f'test time elapsed: {(time() - start) * 100}ms')
-    print(f'test accuracy: {correct * 100 // testsize}%')
+    print(f'OPA test time elapsed: {(time() - start) * 100}ms')
+    print(f'OPA test accuracy: {correct * 100 // testsize}%')
+
+    ### SVM
+
+    clf = svm.SVC(gamma=0.01, C=200.)
+    start = time()
+    clf.fit(X_train, y_train)
+    print(f'SVM train time elapsed: {(time() - start) * 100}ms')
+
+    start = time()
+    h = clf.predict(X_test)
+    print(f'SVM test time elapsed: {(time() - start) * 100}ms')
+    print(f'SVM test accuracy: {int((h == y_test).mean()) * 100}%')
+
+    ### introducing some noise to the training data
+
+    noisesize = 5 * trainsize // 100  # 5% noise
+    noise_indices = random.sample(range(trainsize), noisesize)
+    for i in noise_indices:
+        y_train[i] *= -1
+
+    ### OPA
+
+    clf = OnlinePassiveAgressive(d, tao=2, C=200)
+
+    correct = 0
+    start = time()
+    for i in range(trainsize):
+        yihat = clf.predict(X_train[i])
+        correct += int(y_train[i] == yihat)
+        clf.online_learn(X_train[i], y_train[i])
+
+    print(f'OPA train online accuracy (noise): {correct * 100 // trainsize}%')
+
+    correct = 0
+    start = time()
+    for i in range(testsize):
+        yihat = clf.predict(X_test[i])
+        correct += int(y_test[i] == yihat)
+
+    print(f'OPA test accuracy (noise): {correct * 100 // testsize}%')
