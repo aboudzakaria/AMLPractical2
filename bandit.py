3ed701f728037d9de570cec07e1d830c06a7c747
Aboud <aboud.zakaria@gmail.com>
2018-11-21 15:49:30 +0100

    implement UCB algorithm

--- bandit.py
+++ bandit.py
@@ -1,5 +1,6 @@
 """Multi-Arm Bandit Algorithms."""
 
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
@@ -31,22 +32,26 @@ def pull_arm(arm):
 N_PULLS = 3000
 bandit_pulls = [0 for _ in range(N_ARMS)]
 estimates = np.zeros(N_ARMS)
+total_rewards = 0
 for _ in range(N_PULLS // N_ARMS):
     for arm in range(N_ARMS):
         reward = pull_arm(arm)
         bandit_pulls[arm] += 1
         estimates[arm] += (reward - estimates[arm]) / bandit_pulls[arm]
+        total_rewards += reward
 best_bandit = estimates.argmax()
 print(f"Incremental Uniform:\n\
 bandit pulls={bandit_pulls}, \
 best bandit={best_bandit}, \
-estimate={estimates[best_bandit]}")
+estimate={estimates[best_bandit]}, \
+rewards={int(total_rewards)}")
 
 # epsilon-Greedy
 N_PULLS = 3000
-epsilon = 0.07
+epsilon = 0.4
 bandit_pulls = [0 for _ in range(N_ARMS)]
 estimates = np.zeros(N_ARMS)
+total_rewards = 0
 for _ in range(N_PULLS):
     # choose best arm
     arm = estimates.argmax()
@@ -59,8 +64,39 @@ for _ in range(N_PULLS):
     reward = pull_arm(arm)
     bandit_pulls[arm] += 1
     estimates[arm] += (reward - estimates[arm]) / bandit_pulls[arm]
+    total_rewards += reward
 best_bandit = estimates.argmax()
 print(f"epsilon greedy:\n\
 bandit pulls={bandit_pulls}, \
 best bandit={best_bandit}, \
-estimate={estimates[best_bandit]}")
+estimate={estimates[best_bandit]}, \
+rewards={int(total_rewards)}")
+
+# UCB
+N_PULLS = 3000
+bandit_pulls = [0 for _ in range(N_ARMS)]
+estimates = np.zeros(N_ARMS)
+total_rewards = 0
+for n in range(N_PULLS // N_ARMS):
+    arm = 0
+    max_upper_bound = 0
+    for i in range(N_ARMS):
+        upper_bound = 1e400
+        if bandit_pulls[i] > 0:
+            delta_i = math.sqrt(2 * math.log(n+1) / bandit_pulls[i])
+            upper_bound = estimates[i] + delta_i
+
+        if upper_bound > max_upper_bound:
+            max_upper_bound = upper_bound
+            arm = i
+
+        reward = pull_arm(arm)
+        bandit_pulls[arm] += 1
+        estimates[arm] += (reward - estimates[arm]) / bandit_pulls[arm]
+        total_rewards += reward
+best_bandit = estimates.argmax()
+print(f"UCB:\n\
+bandit pulls={bandit_pulls}, \
+best bandit={best_bandit}, \
+estimate={estimates[best_bandit]}, \
+rewards={int(total_rewards)}")
